489a59fc58766a891bf40e639273d68024c87340
Wondwosen Desalegn <wonde.desalegn@gmail.com>
2017-02-07 12:21:58 +0300

    API for accessing all AD subpages

--- models/subPageModel.js
+++ models/subPageModel.js
@@ -4,7 +4,7 @@ var DbHelper = require('../lib/DbHelper');
 var Util = require('../lib/util');
 
 var subPageModel = {
-
+    //Get all daily deal subpages
     getSubPages: function(){
         return new Promise(function(resolve, reject) {
             DbHelper.getConnection().then(function(connection){
@@ -31,7 +31,26 @@ var subPageModel = {
 
             
         });
-    }
+    },
+    //Gets all ad subpages
+    getAdSubpages: function() {
+        return new Promise(function(resolve, reject) {
+            DbHelper.getConnection().then(function(connection) {
+                connection.query('Select * from ppc_subpages',
+                    function(err, rows, fields) {
+                        connection.release();
+                        if(err) {
+                            reject(err);
+                        }
+                        resolve(rows);
+                    }
+                );
+            }, function(error) {
+                if(error)
+                    reject(new Error('Connection error'));
+            });
+        });
+    },
 }
 
 module.exports = subPageModel;
