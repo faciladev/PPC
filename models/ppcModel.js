71c956110cb2b3367c90133c49cfab7c5a452811
Fasil Girma <abbifa@gmail.com>
2017-02-08 15:20:33 +0300

    Connection pooling error fix on daily deal listing page.

--- models/ppcModel.js
+++ models/ppcModel.js
@@ -238,57 +238,52 @@ var ppcModel = {
     getDealsByCategory : function(categoryId, page){
         
         return new Promise(function(resolve, reject) {
-            DbHelper.getConnection().then(function (connection) {
 
-                var query = 
-                'SELECT ' + 
-                'dd.id AS deal_id, ' +
-                'm.id AS microsite_id, '+
-                'm.company_name, ' +
-                'm.what_you_get, '+
-                'm.location,' +
-                'dd.end_date, ' +
-                'dd.start_date, ' +
-                'm.discount_daily_description, '+
-                'm.discount_percentage, '+
-                'dd.discount_type, '+
-                'm.name, '+
-                'dd.discount_price, ' +
-                'm.image, ' +
-                'm.image_1, ' +
-                'm.image_2, ' +
-                'm.code, ' +
-                'dd.date_created, ' +
-                'dd.download_price, ' +
-                'm.discount_description, ' +
-                'dd.regular_price, '+
-                'dd.discount_rate, '+
-                'dd.coupon_name, '+
-                'dd.coupon_generated_code, '+
-                'm.image, '+
-                'dd.is_approved, ' +
-                'dd.is_deleted, ' +
-                'dd.list_rank, ' +
-                'dd.deal_image, ' +
-                'm.discount_description, '+
-                'm.daily_deal_description, '+
-                'dd.approved_category_id '+
-                'FROM ppc_daily_deal AS dd LEFT JOIN ppc_deal_microsites ' +
-                'AS m ON dd.daily_deal_microsite_id=m.id ' +
-                'WHERE dd.is_deleted=0 AND dd.is_approved=1 AND dd.approved_category_id=' +
-                categoryId ;
-                PaginationHelper.paginate(query, page).then(
-                    function(response){
-                        resolve(response);
-                    }, 
-                    function(error){
-                        reject(error);
-                    }
-                );
-            },
-            function(error){
-                reject(error);
-            });
+            var query = 
+            'SELECT ' + 
+            'dd.id AS deal_id, ' +
+            'm.id AS microsite_id, '+
+            'm.company_name, ' +
+            'm.what_you_get, '+
+            'm.location,' +
+            'dd.end_date, ' +
+            'dd.start_date, ' +
+            'm.discount_daily_description, '+
+            'm.discount_percentage, '+
+            'dd.discount_type, '+
+            'm.name, '+
+            'dd.discount_price, ' +
+            'm.image, ' +
+            'm.image_1, ' +
+            'm.image_2, ' +
+            'm.code, ' +
+            'dd.date_created, ' +
+            'dd.download_price, ' +
+            'm.discount_description, ' +
+            'dd.regular_price, '+
+            'dd.discount_rate, '+
+            'dd.coupon_name, '+
+            'dd.coupon_generated_code, '+
+            'm.image, '+
+            'dd.is_approved, ' +
+            'dd.is_deleted, ' +
+            'dd.list_rank, ' +
+            'dd.deal_image, ' +
+            'm.discount_description, '+
+            'm.daily_deal_description, '+
+            'dd.approved_category_id '+
+            'FROM ppc_daily_deal AS dd LEFT JOIN ppc_deal_microsites ' +
+            'AS m ON dd.daily_deal_microsite_id=m.id ' +
+            'WHERE dd.is_deleted=0 AND dd.is_approved=1 AND dd.approved_category_id=' +
+            categoryId ;
+            PaginationHelper.paginate(query, page).then(
+                function(response){
+                    resolve(response);
+                }, 
+                function(error){
+                    reject(error);
+                }
+            );
         });
     },
 
