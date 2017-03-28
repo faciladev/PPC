d14b98e7c6f2d2c8c294f2c9208a721b071f7541
Fasil Girma <abbifa@gmail.com>
2017-03-28 12:43:21 +0300

    Cron job configuration added for development and production

--- routes/logs.js
+++ routes/logs.js
@@ -2,17 +2,20 @@ var express = require('express');
 
 const Joi = require('joi');
 var jsonfile = require('jsonfile');
+var config = require('config');
 
 var logger = require('../logger');
 var appError = require('../app_error');
 
-var file = __dirname + '/../logs/log-email.json';
+var file = config.get('log_path') + '/log-email.json';
 
 var router = express.Router();
 
 //return logs of :days days including today
 router.get('/', function(req, res, next){
-
+	// if(x.length){
+	// 	y=3;
+	// }
     jsonfile.readFile(file, function(err, data){
         if(err){
             return next(err);
