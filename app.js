6759886bb2c22edfdbd72be5a7dd985088b3dc40
Fasil Girma <abbifa@gmail.com>
2017-04-05 11:53:18 +0300

    Removed unnecessary console logs.

--- app.js
+++ app.js
@@ -51,9 +51,9 @@ app.use(compression());
 app.use(cors(corsOptionsDelegate));
 
 //Enable pre-flight mode
-app.options('*', cors()) 
-
-app.use(morgan('dev'));
+app.options('*', cors()); 
+//logger for non-production environments only
+app.get("env") !== "production" && app.use(morgan('dev'));
 app.use(fileUpload());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
